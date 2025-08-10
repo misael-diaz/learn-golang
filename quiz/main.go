7f4805cf4e0911beac0bc1c2118aab0b7663ff80
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-08-10 15:48:15 -0400

    completes quiz app
    NOTES
    used string comparison is not case sensitive
    we hope that `atoi()` is just a wrapper for the stdlibc implementation
    so it should work in most cases without error, that's why we ignored
    the error returned in those cases

--- quiz/main.go
+++ quiz/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"bufio"
 	"encoding/csv"
+	"strings"
+	"strconv"
 );
 
 func main() {
@@ -18,18 +20,31 @@ func main() {
 	if (nil != err) {
 		log.Fatal(err);
 	}
+	ncorrect := 0;
 	nproblems := len(records);
 	for _, ln := range records {
 		problem_type, problem_statement, problem_answer := ln[0], ln[1], ln[2];
 		fmt.Printf("prompt: %s\n", problem_statement);
 		response, err := console_reader.ReadString('\n');
+		res := strings.TrimSpace(response);
 		if (nil != err) {
 			log.Fatal(err);
 		}
-		fmt.Println(problem_type);
-		fmt.Println(problem_answer);
-		fmt.Println(response);
+		if (0 == strings.Compare("text", problem_type)) {
+			if (0 == strings.Compare(res, problem_answer)) {
+				ncorrect++;
+			}
+		} else if (0 == strings.Compare("math", problem_type)) {
+			ans, _ := strconv.Atoi(problem_answer);
+			num, _ := strconv.Atoi(res);
+			if (ans == num) {
+				ncorrect++;
+			}
+		} else {
+			log.Fatal("UXProblemTypeError");
+		}
 	}
-	fmt.Println(nproblems);
+	fmt.Printf("total number of problems: %d\n", nproblems);
+	fmt.Printf("total number of correct answers: %d\n", ncorrect);
 	file.Close();
 }
